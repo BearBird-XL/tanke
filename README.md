0aa120076e04bdfe481a743cbd48614ad853b171
lei <xionglei1614@163.com>
2022-04-14 00:38:00 +0800

    fix： 整理仓库

--- README.md
+++
@@ -1,4 +0,0 @@
-# 设计模式-坦克
-> 学习自马士兵坦克大战 
-> https://ke.qq.com/course/398245?tuin=b09cbb87#term_id=100475004
-
--- src/main/java/xyz/xionglei/tanke/v4/Dir.java
+++
@@ -1,9 +0,0 @@
-package xyz.xionglei.tanke.v4;
-
-/**
- * @author xionglei
- * @create 2021-09-09 10:54
- */
-public enum Dir {
-    UP, DOWN, LEFT, RIGHT, STOP
-}
