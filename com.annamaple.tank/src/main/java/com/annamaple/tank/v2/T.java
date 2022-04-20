5d88bac1d70665104a267b9f6eb00b8a2528ab6e
熊雷 <xionglei@jiuqi.com.cn>
2022-04-20 20:21:19 +0800

    add(v2): 继承Frame重写paint

---
+++ com.annamaple.tank/src/main/java/com/annamaple/tank/v2/T.java
@@ -0,0 +1,14 @@
+package com.annamaple.tank.v2;
+
+import java.awt.*;
+
+/**
+ * @author xionglei
+ * @create 2022-04-20 20:02
+ */
+public class T {
+
+    public static void main(String[] args) {
+        Frame f = new TankFrame();
+    }
+}
---
+++ com.annamaple.tank/src/main/java/com/annamaple/tank/v2/TankFrame.java
@@ -0,0 +1,44 @@
+package com.annamaple.tank.v2;
+
+import cn.hutool.core.lang.Console;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * 集成Frame,重写Frame
+ *
+ * @author xionglei
+ * @create 2022-04-20 20:11
+ */
+public class TankFrame extends Frame {
+
+    /**
+     * 无参构造
+     */
+    public TankFrame() throws HeadlessException {
+        this.setSize(800, 600);
+        // 设置不能改变大下
+        this.setResizable(false);
+        // 设置标题
+        this.setTitle("tank war");
+        // 设置可见
+        this.setVisible(true);
+        // 设置点击关闭按钮程序退出
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Console.log("paint");
+        // 填充一个矩形
+        g.fillRect(200, 200, 50, 50);
+    }
+}
